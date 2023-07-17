8783669f5dd1895ce173f883b6ceb93bbba44b4c
kleek6 <103236704+kleek6@users.noreply.github.com>
2023-07-17 23:24:41 +0500

    Update config.py

--- util/config.py
+++ util/config.py
@@ -5,9 +5,9 @@ DEBUG = True
 # Токены VK
 # НЕ ЗАЛИВАТЬ В ГИТХАБ!!!!!!!
 # Токен сообщества для авторизации
-GROUP_TOKEN = 'vk1.a.iPTjNSIGYwOJ2uaetVVI_ISc0onI5qiSrkiz3rioWiKpRng7SmGFiy7YzCzN34MV9Xl0LfUhQ0jtZ7VAboEvWhPX0XHtzFiVIjPoAlJVnd8H7KI-o6x1csHiGAtDm8rjiVyhlpoxY0aYEkwTiiTyvapiiog3NFbSdwmX8nMziWCd-tl2bXvBQnHv3MDuoJCRsOfmDwu81FTXwLsg8hp8sg'
+GROUP_TOKEN = ''
 # Токен пользователя имеющего права на передачу и чтение фоток ВК
-USER_TOKEN = 'vk1.a.L4jZvEUzi5PBMLvOJ2omAd0vtPi_TWZHGF5-qxVN2FsLnRGI0ygZfeOie_ATNlfaZkeGpI4cnj4ZLPMbcALvuTzgjL4ynqVbTameDSmCoU6weV1tdcwZpzy-IYo9sf4yWuIQJQwy7WuNm9tOh6u-zLG44LdAtwUOW845hyg_84aWm5LyV3cD13K3CX_U09Vb88WPeh7M50yBc9haklB8Hw'
+USER_TOKEN = ''
 
 # Подключение к ДБ
 DB_HOST = "localhost"
